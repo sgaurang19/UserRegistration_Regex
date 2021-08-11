ba852a8b56a08014c11722e8c0a1c0b0516d082c
Gaurang <shirsatgd@gmail.com>
2021-08-11 21:44:14 +0530

    Committed

---
+++ src/user_registrartion.js
@@ -0,0 +1,12 @@
+function checkName(name1){
+    var regexName = /^[A-Z ][a-z]{2,30}$/;
+    var result = regexName.test(name1);
+    if(result == 1){
+        console.log("Valid Name");
+    }else{
+        console.log("Please Enter Valid name");
+    }
+}
+
+var firstName = "Gaurang";
+checkName(firstName);
