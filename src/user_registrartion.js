1e16eb80db6702d62984b97b43a8633b26423fe0
Gaurang <shirsatgd@gmail.com>
2021-08-11 21:59:57 +0530

    UC3 committed for Valid Email

--- src/user_registrartion.js
+++ src/user_registrartion.js
@@ -15,3 +15,17 @@ checkName(firstName);
 //UC2 : Valid Last Name
 var lastName = "Shirsat";
 checkName(lastName);
+
+// UC3 : Valid Email
+
+function checkEmail(em){
+    var regexEmail = /^([a-zA-Z0-9._]+)(@)([a-zA-Z0-9]+)(.)([a-zA-Z]+)([.]+?)([a-zA-Z]+?)$/;
+    var result = regexEmail.test(em);
+    if(result == 1){
+        console.log("Valid Email");
+    }else{
+        console.log("Please Enter Valid Email");
+    }
+}
+var email = "abc.xyz@bl.co.in";
+checkEmail(email);
